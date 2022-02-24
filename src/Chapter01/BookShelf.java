cc2ae60d7aec6785fb3276ba9e985b66d2e06df4
hanbee <sonhanbi1002@gmail.com>
2022-02-24 17:21:55 +0900

    서가의 크기를 지정하지 않고 책을 넣을 수 있도록 array를 list로 변경

--- src/Chapter01/BookShelf.java
+++ src/Chapter01/BookShelf.java
@@ -1,19 +1,22 @@
 package Chapter01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookShelf implements Aggregate {
-    private Book[] books;
+    private List<Book> books;
     private int last = 0;
 
-    public BookShelf(int maxSize) {
-        this.books = new Book[maxSize];
+    public BookShelf() {
+        this.books = new ArrayList<>();
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
+        this.books.add(book);
         last++;
     }
 
