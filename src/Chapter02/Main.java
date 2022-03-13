dbbebcf1389a69f0bb11b548ebfdf54e22167cbe
hanbee <sonhanbi1002@gmail.com>
2022-03-13 20:00:09 +0900

    연습문제 구현

--- src/Chapter02/Main.java
+++ src/Chapter02/Main.java
@@ -1,5 +1,7 @@
 package Chapter02;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Print p = new PrintBanner("Hello");
@@ -9,5 +11,16 @@ public class Main {
         Print2 p2 = new PrintBanner2("World");
         p2.printWeak();
         p2.printStrong();
+
+        try {
+            FileIO fileIO = new FileProperties();
+            fileIO.readFromFile("file.txt");
+            fileIO.setValue("year", "2022");
+            fileIO.setValue("month", "3");
+            fileIO.setValue("day", "13");
+            fileIO.writeToFile("newfile.txt");
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
     }
 }
